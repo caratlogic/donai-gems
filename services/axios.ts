ffcd9e47d0b1a69c878d6801fded209f5b45ca97
Uday Singh <19.yudee@gmail.com>
2025-09-01 01:07:40 +0530

    changed env url

--- services/axios.ts
+++ services/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
     process.env.NEXT_PUBLIC_API_BASE_URL ||
-    "https://api-gems-inventory.onrender.com/api";
+    "https://api-gems-inventory.onrender.com";
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
