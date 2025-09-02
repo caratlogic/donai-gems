ecb896f1931008c0f45402d5e6e236f239c22a16
Uday Singh <19.yudee@gmail.com>
2025-09-02 16:53:14 +0530

    removed wrong env variable in gems.ts

--- services/gems.ts
+++ services/gems.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Gem } from "@/lib/validations/gems-Schema";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export interface GemsApiResponse {
     success: boolean;
